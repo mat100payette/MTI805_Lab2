2af746c1b08cd044b0aea196afc9e39d1f58dacd
Alex Gosselin-Pronovost <agosselin@symbotic.com>
2018-02-04 14:59:34 -0500

    Added stitching.

--- Emgu_Panorama/frmMain.Designer.cs
+++ Emgu_Panorama/frmMain.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(916, 475);
+            this.ClientSize = new System.Drawing.Size(239, 55);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.Name = "frmMain";
--- Emgu_Panorama/frmMain.cs
+++ Emgu_Panorama/frmMain.cs
@@ -11,14 +11,26 @@ using Emgu.CV;
 using Emgu.CV.UI;
 using Emgu.CV.Structure;
 using Emgu.CV.Util;
+using Emgu.CV.Stitching;
 
 namespace Emgu_Panorama
 {
     public partial class frmMain : Form
     {
+        Mat result = new Mat();
+        Stitcher stitcher = new Stitcher(false);
+        Mat image1 = new Mat("C:/Users/agosselin/Desktop/MTI805_Lab2/Boat/boat1.jpg");
+        Mat image2 = new Mat("C:/Users/agosselin/Desktop/MTI805_Lab2/Boat/boat2.jpg");
+        Mat image3 = new Mat("C:/Users/agosselin/Desktop/MTI805_Lab2/Boat/boat3.jpg");
+        Mat image4 = new Mat("C:/Users/agosselin/Desktop/MTI805_Lab2/Boat/boat4.jpg");
         public frmMain()
         {
             InitializeComponent();
+            VectorOfMat vmImage = new VectorOfMat(image1, image2, image3, image4);
+            stitcher.Stitch(vmImage, result);
+
+            ImageViewer.Show(result);
         }
+
     }
 }
